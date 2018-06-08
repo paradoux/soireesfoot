e1beb9c6cbc1aa02fd051b72c0774de3a9c336e2
Axel Martin <martin.axe@live.fr>
2018-06-08 18:03:50 +0200

    Setup env var

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import App from './App';
 import Client from 'shopify-buy';
 
 const client = Client.buildClient({
-    storefrontAccessToken: process.env.STORE_TOKEN,
-    domain: process.env.DOMAIN
+    storefrontAccessToken: 'af3d0fb0b6fc6a7fcb5d15fa353293b5',
+    domain: 'soireesfoot.myshopify.com'
 });
 
 
